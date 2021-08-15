b2191f09be8cd8b0ed0382a284b8dd05828ac2c7
puqiqi <1186808746@qq.com>
2021-08-15 13:26:31 +0800

    code one

---
+++ code/test_8_14/test_8_14/test.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//#include<stdio.h>
+
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	int sum = 0;
+//	scanf("%d%d", &num1, &num2);
+//	sum = num1 + num2;
+//	printf("sum=%d\n", sum);
+//	return 0;
+//}
+
+#include<stdio.h>
+
+int main()
+{
+	int num = 4;
+	printf("%d\n", num);
+	num = 8;
+	printf("%d\n", num);
+
+	return 0;
+}
